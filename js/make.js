e7a34fc256335e733958787b1def17137a11ceba
njuhi <morphynjuh@mail.com>
2019-02-15 18:36:14 +0300

    save changes

--- js/make.js
+++ js/make.js
@@ -1,27 +1,15 @@
-var equilateral= function(s1,s2 ,s3) {
-  return s1 , s2,s3
-};
-
-var number1 = parseInt(prompt("Enter a number to divition:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = divition(number1, number2);
-alert(result);
-var multiplication = function(number1, number2) {
-  return number1 * number2;
-};
-
-var number1 = parseInt(prompt("Enter a number to multiplication :"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = multiplication(number1, number2);
-alert(result);
-var add = function(number1, number2) {
-  return number1 + number2;
-};
-
-var number1 = parseInt(prompt("Enter a number to add:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
-alert(result);
-var subtraction = function(number1, number2) {
-  return number1 - number2;
-};
+//******************************
+var triangle = function() {
+  var base = parseInt(document.getElementById('base').value);
+  var side = parseInt(document.getElementById('side').value);
+  var hypotenuse = parseInt(document.getElementById('hypotenuse').value);
+  if
+  ( base == side && base == hypotenuse);
+    {
+      alert("equilateral");
+    }
+    else if (base<side || side>hypotenuse || hypotenuse<side);
+    {
+      alert("not equilateral");
+    }
+  }
