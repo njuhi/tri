10e73201730c7ab2e49c6fb92e0c5fb3699a6d45
njuhi <morphynjuh@mail.com>
2019-02-16 16:50:49 +0300

    save changes

--- js/make.js
+++ js/make.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var triangle = function() {
   var base = parseint(document.getElementById('base').value);
   var side = parseint(document.getElementById('side').value);
@@ -6,25 +5,8 @@ var triangle = function() {
      if( base == side && side == hypotenuse);
       {
          alert("equilateral");
-       }
-      else (base==side || side<hypotenuse || hypotenuse>base);
+       }else (base==side || side<hypotenuse || hypotenuse>base);
       {
         alert("isoceles");
       }
-    };
-=======
-//******************************
-var triangle = function() {
-  var base = parseint(document.getElementById('base').value);
-  var side = parseint(document.getElementById('side').value);
-  var hypotenuse = parseint(document.getElementById('hypotenuse').value);
-    if( base == side && base == hypotenuse);
-    {
-      alert("equilateral");
-    }
-     else (base==side || side<hypotenuse || hypotenuse>base);
-    {
-      alert("isoceles");
-    }
-  };
->>>>>>> triangle-tracker
+     };
