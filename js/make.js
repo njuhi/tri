4c5508f9148e4e7ca576b5d2815a2dc52f68b05d
njuhi <morphynjuh@mail.com>
2019-02-16 16:05:42 +0300

    save changes

--- js/make.js
+++ js/make.js
@@ -2,12 +2,12 @@ var triangle = function() {
   var base = parseint(document.getElementById('base').value);
   var side = parseint(document.getElementById('side').value);
   var hypotenuse = parseint(document.getElementById('hypotenuse').value);
-  if( base == side && side == hypotenuse);
-    {
-      alert("equilateral");
-    }
-    else (base==side || side<hypotenuse || hypotenuse>base);
-    {
+     if( base == side && side == hypotenuse);
+      {
+         alert("equilateral");
+       }
+      else (base==side || side<hypotenuse || hypotenuse>base);
+      {
       alert("isoceles");
-    }
-  }
+      }
+    };
