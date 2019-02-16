ed68f8333aa846d10303cd1b0430a9e780530821
njuhi <morphynjuh@mail.com>
2019-02-16 15:56:43 +0300

    save changes

---
+++ js/make.js
@@ -0,0 +1,14 @@
+var triangle = function() {
+  var base = parsefloat(document.getElementById('base').value);
+  var side = parsefloat(document.getElementById('side').value);
+  var hypotenuse = parsefloat(document.getElementById('hypotenuse').value);
+  if
+  ( base == side && base == hypotenuse);
+    {
+      alert("equilateral");
+    }
+    else (base==side || side<hypotenuse || hypotenuse>base);
+    {
+      alert("isoceles");
+    }
+  }
