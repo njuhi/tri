f8a18b2585a23daee1a62f49be3c85e8992ecb6e
njuhi <morphynjuh@mail.com>
2019-02-17 20:13:56 +0300

    save deleted code and images

--- js/make.js
+++ js/make.js
@@ -1,8 +1,1 @@
-var triangle = function() {
-  var base = parseint(document.getElementById('base').value);
-  var side = parseint(document.getElementById('side').value);
-  var hypotenuse = parseint(document.getElementById('hypotenuse').value);
-     if( base == side && side == hypotenuse);
-      {
-         alert("equilateral");}
-       }
+
