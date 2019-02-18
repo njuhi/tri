796b28bf5aec0a4d50a2d18f0ddb21d8b1eff4ac
njuhi <morphynjuh@mail.com>
2019-02-18 16:47:06 +0300

    save changes

--- js/make.js
+++ js/make.js
@@ -1,6 +1,14 @@
-var triangle = s1,s2,s3;
+var triangle = function(){
+    var s1 = parseInt(document.getElementById('s1').value);
+    var s2 = parseInt(document.getElementById('s2').value);
+    var s3 = parseInt(document.getElementById('s3').value);
 
-if (triangle >= s1==s2 && triangle>= s2==s3){
+  if(s1==s2 && s2==s3 && s3==s1){
+        alert("equilateral");
+    }
+    else {
 
-  document.write("equilateral!")
+    alert("not an equilateral");
+  }
 }
+triangle();
